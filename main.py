f3752312f1f485061c1e9ab22ca35fce6a754539
ultroi <86416271+ultroi@users.noreply.github.com>
2021-12-12 22:19:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def start(_,message):
   
 @bot.on_message(filters.command('request'))
 def req(_,message):
-    query = " ".join(args)
+    query = " "
     if not query: 
         message.reply("Please enter a query!") 
         return
