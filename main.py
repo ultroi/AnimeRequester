ffde8391877934c4e895afd7d3e31c108d016841
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-12-12 15:50:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,9 +25,10 @@ def call_back_in_filter(data):
 
 @bot.on_message(filters.command('start'))
 def start(_,message):
-    message.reply_text(text =f"""Hello there , use /request your ideas - to request""")
     keyboard = []
     keyboard.append([InlineKeyboardButton("Request" , callback_data=f"request:{message.from_user.id}")])
+    message.reply_text(text =f"""Hello there , use /request your ideas - to request""" , reply_markup=InlineKeyboardMarkup(keyboard))
+    
  
 @bot.on_message(filters.command('request'))
 def req(_,message):
