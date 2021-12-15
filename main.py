2135933b9c33a3e4d5c5f608c24812d9fa16e1d0
ultroi <86416271+ultroi@users.noreply.github.com>
2021-12-15 21:36:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def start(_,message):
     message.reply_text(text =f""" **Welcome To Anime Publish Bot** \n Hello dear,\nTell your request, query and other prblm related to channel. You tell directly to channel admins to resolve problem.\n\n Use = `/request <query>` """ , reply_markup=InlineKeyboardMarkup(keyboard))
   
 @bot.on_message(filters.command('request'))
-def req(_,message):
+def req_(update: Update, context: CallbackContext):
     message = update.effective_message
     search = message.text.split(" ", 1)
     if len(search) == 1:
