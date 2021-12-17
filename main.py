f34b1094b69a652ba7c491dec863568dfcb3ddef
ultroi <86416271+ultroi@users.noreply.github.com>
2021-12-18 00:00:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from pyrogram import filters, Client
+from telegram import filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Update 
 import requests
 import os  
