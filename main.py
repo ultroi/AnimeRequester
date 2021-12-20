811fcbca563670504aaa084c037531cc761b66fb
ultroi <86416271+ultroi@users.noreply.github.com>
2021-12-20 14:49:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import html
 import textwrap 
 import jikanpy 
 import bs4 
-from telegram import filters, Client
+from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Update 
 from telegram.ext import CallbackContext, run_async
 from telegram import ParseMode 
