85ac63e52d94b1bdaf7fef3aa78ce4fb95d3016f
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-12-20 21:19:42 +0700

    main: refactor

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import requests
-import os  
+import os
 import aiohttp
-import html 
-import textwrap 
-import jikanpy 
-import bs4 
+import html
+import textwrap
+import jikanpy
+import bs4
 from pyrogram import filters, Client
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Update 
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import CallbackContext, run_async
-from telegram import ParseMode 
-from alternation import typing_action 
+from telegram import ParseMode
+from alternation import typing_action
 
 bot = Client(
     "notesbot",
     api_id=os.environ['API_ID'],
     api_hash=os.environ['API_HASH'],
     bot_token=os.environ['BOT_TOKEN'],
-    
+
 )
 
 CHAT_ID = os.environ.get('CHAT_ID')
@@ -30,7 +30,6 @@ def call_back_in_filter(data):
     )
 
 
-
 @bot.on_message(filters.command('start'))
 def start(_,message):
     keyboard = []
@@ -38,21 +37,21 @@ def start(_,message):
     keyboard.append([InlineKeyboardButton("Anime Channel", url="https://t.me/Anime_Publish")])
     keyboard.append([InlineKeyboardButton("Anime Movies", url="https://t.me/AnimeNexusMovies")])
     message.reply_text(text =f""" **Welcome To Anime Publish Bot** \n Hello dear,\nTell your request, query and other prblm related to channel. You tell directly to channel admins to resolve problem.\n\n Use = `/request <query>` """ , reply_markup=InlineKeyboardMarkup(keyboard))
-  
+
 
 @bot.on_message(filters.command('request'))
-async def req_(_, message):
+def req_(_, message):
     if len(message.command) != 2:
-        message.reply_text("Format : /query < query >") 
-        return  
-    else: 
+        message.reply_text("Format : /query < query >")
+        return
+    else:
         global req_
     req_ = message.text.replace(message.text.split(' ')[0] , '')
     keyboard = []
     keyboard.append([InlineKeyboardButton("✅ Accept" , callback_data=f"request:accept:{message.from_user.id}")])
     keyboard.append([InlineKeyboardButton("❌ Reject" , callback_data=f'request:reject:{message.from_user.id}')])
     bot.send_message(int(CHAT_ID) , f'#Request \n\n **• Requestor Info:-\n ID - {message.from_user.id} \n Username - @{message.from_user.username} \n\n Requested for** -{req_}' , reply_markup=InlineKeyboardMarkup(keyboard))
-    
+
 
 @bot.on_callback_query(call_back_in_filter('request'))
 def botreq(_,query):
@@ -65,11 +64,9 @@ def botreq(_,query):
     elif result[1] == "reject" and query.from_user.id == owner:
         bot.send_message(result[2] , "Sorry your Request has been decline. Please check [Anime List](https://t.me/Anime_Publish/3041) ! ")
         query.message.edit('Rejected!')
-    
+
     else:
         query.answer('You are not allowed')
-
-
 
 
 
