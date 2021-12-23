d3005fd6470c5de69a1e7fcdc5db455cb312301c
ultroi <86416271+ultroi@users.noreply.github.com>
2021-12-23 13:32:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,8 @@ def call_back_in_filter(data):
 
 @bot.on_message(filters.command('start'))
 def start(_,message): 
-    keyboard = []
+    keyboard = [] 
+    keyboard.append([InlineKeyboardButton ("Our Channel", url="https://t.me/Anime_Publish")])
     message.reply_text(text =f""" **Hello dear**,\n\n Tell your request, query and other prblm related to channel. You tell directly to channel admins to resolve problem.\n\n Use = /request (query) """ , reply_markup=InlineKeyboardMarkup(keyboard))
 
 
