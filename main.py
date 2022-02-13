71b1cd3f674956b8f064ce714410fbb4381d48ea
ultroi <86416271+ultroi@users.noreply.github.com>
2022-02-13 20:55:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -77,9 +77,9 @@ from pyrogram import Client, filters
 app = Client("my_account")
 
 
-    @app.on_message(filters.private)
-async def hello(client, message):
-    await message.reply("Hello from Pyrogram!")
+@app.on_message(filters.private)
+def hello(client, message):
+    message.reply("Hello from Pyrogram!")
 
      
         
